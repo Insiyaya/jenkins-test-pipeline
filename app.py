a5289d7207a1d0ef368755a0f9e289c5f6af72d9
insiya <insiya.h@consultadd.com>
2025-08-04 10:39:47 +0530

    linting

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ print("Final line to confirm the changes are reflected in the app.py file.")
 
 print("This is a test for the webhoo from GitHub.")
 
-print("Webhook trigger test s")
+print("Webhook trigger test successful")
