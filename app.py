2ab0fed40a7e0afd11c68fd58a3ec5b838925495
insiya <insiya.h@consultadd.com>
2025-08-04 10:38:47 +0530

    linting

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ print("This is a sample Python application....")
 print("Testing github webhook trigger...")
 print("This is a new line added to the app.py file.")
 print("This line is added to test the webhook trigger from GitHub.")
-print("Another line to ensure the webhook is working correctly.")
+print("Another line to ensure thes working correctly.")
 print("Final line to confirm the changes are reflected in the app.py file.")
 
-print("This is a test for the webhook trigger from GitHub.")
+print("This is a test for the webhoo from GitHub.")
 
-print("Webhook trigger test successful.")
+print("Webhook trigger test s")
